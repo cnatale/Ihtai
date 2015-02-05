2cd5bfcd4c233b25238d0fc41d1654da89687ff7
cnatale <Chris.Natale@gridpoint.com>
2015-02-05 10:58:15 -0800

    code revisions

--- js/ihtai.js
+++ js/ihtai.js
@@ -16,7 +16,7 @@ TODO:
 
 var Ihtai = (function(){
 
-	var numClusters, ioStimuli, memorizer;
+	var numClusters, ioStimuli, memorizer, intervalID;
 
 	function init(bundle){
 		if(typeof bundle == "undefined")
@@ -53,6 +53,13 @@ var Ihtai = (function(){
 
 
 		console.log('init clusters: '+ k);
+
+		//intervalID = window.setInterval(cycle, 33); //initiate cycle
+	}
+
+	function cycle(){
+		//TODO: cycle through memorization, drives, reflexes, and iostimuli in correct order
+
 	}
 
 
@@ -67,50 +74,111 @@ var Ihtai = (function(){
 	moments in time represented by vectors combining stimuli and drive states.
 */
 var Memorizer = (function(_height){
-
-	var height=_height, acceptableRange=1 level;
+	var height=_height, acceptableRange=1, level, homeostasisGoal;
 
 	function init(){
 		//initialize a 2d array representing all possible memories
 		level=[];
 		for(var i=0; i<height; i++){
-			level[i]=[];
+			level[i].series={};
+			level[i].recordingSeries={};
+			level[i].ctr =0; //initialize time sequence counter for each level
 		}
+
+		//TODO: set a real homeostasisGoal
+		homeostasisGoal=[0,0,0,0,0];
 	}
 	init();
 
 	/**
-		Takes a vector representing current i/o stimuli state combined with current drive state
+		Takes a cluster containing vector representing current i/o stimuli state combined with current 
+		drive state.
 		@returns a vector representing the moment output stimuli for moment in time 
 		most likely to satiate current drives, i.e. the behavior most likely to satiate drives.
 	*/
-	function cycle(currStimuli){
+	function query(cluster){
 		var outputStimuli, stimDist, sd;
 
 		for(var i=0; i<height; i++){
-			//TODO: go through a level, finding closest match to stimuli
-			//TODO: initial implementation can be O(n), but implement as a sorted
-			// O(log(n)) match for performance.
-			for(var j=0; j< level[i].length; j++){
-				//TODO: this isn't right
-				sd=sqDist(level[i][j], currStimuli);
-				if(sd<outputStimuli)
-					outputStimuli=sd;
-			}
+			//At each level, compare time series' end drive state with homeostasis goal.
+			//If result < acceptable range, return time series' starting ouput stimuli (what agent will act on).
+			//TODO: define homeostasisGoal
+			sd = sqDist(level[i].series[cluster.id].endState, homeostasisGoal);
 
-
-			//TODO: if no match is within acceptable range, go to next level
-			if(stimDist <= acceptableRange)
+			//if no match is within acceptable range, go to next level
+			if(sd <= acceptableRange){
+				outputStimuli = level[i].series[cluster.id].secondState;
 				break;
+			}
 		}
-
 
 
 		return outputStimuli;
 	}
 
+	function sqDist(v1, v2){
+		var d=0;
+		for(var i=0; i<v1.length;i++){
+			d+=Math.pow(v1[i] - v2[i], 2);
+		}
+
+		return d;
+	}
+
+	/**
+		Memorizes stimuli
+	*/
+	function memorize(cluster){
+		/*
+			Loop through each time level. At level i, a memory series is i+2 moments long. 
+			The only moments we need to store in the series are the start and end moments, though.
+			
+			Every level has a counter, that is reset every time a new memory sequence starts. The memory
+			sequence counts to i+2	
+		*/
+		var sd1,sd2;
+		stimuli=cluster.stimuli;
+		for(var i=0; i<height; i++){
+
+			if(level[i].ctr==0){
+				//start tracking new memory series
+				level[i].recordingSeries.startState=stimuli;
+				level[i].recordingSeries.clusterId=cluster.id; //key series based on startState cluster id
+			}
+			//TODO: add in signal after first, which is the one that will be returned
+			if(level[i].ctr == 1){
+				level[i].recordingSeries.secondState=stimuli;
+			}
+			if(level[i].ctr == i+2){
+				level[i].recordingSeries.endState=stimuli;
+				/*
+				TODO: If series' end state is less different from homeostasis goal than   
+				current series stored at this start state, overwrite.
+				*/
+				sd1 = sqDist(level[i].recordingSeries.endState, homeostasisGoal);
+				sd2 = sqDist(level[i].series[level[i].recordingSeries.clusterId].endState, homeostasisGoal);
+
+				if(sd1 < sd2){
+					//add memory series to level. Hash based on starting state cluster id.
+					level[i].series[level[i].recordingSeries.clusterId]={
+						startState: level[i].recordingSeries.startState, 
+						secondState: level[i].recordingSeries.secondState,
+						endState: level[i].recordingSeries.endState
+					};					
+				}
+
+				level[i].ctr=0;
+			}
+			else{
+				
+				level[i].ctr++;
+			}
+		}
+	}
+
 	return {
-		cycle: cycle
+		query: query,
+		memorize: memorize
 	}
 });
 
@@ -189,18 +257,52 @@ var IoStimuli = (function(){
 	};
 });
 
-//drives are internal stimuli determined by algorithms that take each other and external stimuli as inputs
-var Drives = (function(){
+/**
+	Drives are internal stimuli with states determined by algorithms that take each other and external stimuli 
+	as inputs. Each drive contains a method which maps io stimuli and other drive states into an output
+	drive state.
+
+	@params drives: Array. An array of drive methods.
+*/
+var Drives = (function(_drives){
+	var drives = _drives;
+	function init(){
+
+	}
+	init();
+
+	function cycle(){
+		for(var i=0;i<drives.length;i++){
+			//TODO:execute each method in drives once per cycle
+			drives[i]();
+		}
+	}
 
 	return {
-
+		cycle:cycle
 	};
 });
 
-//instincts are actions triggered when no memory series matches desired drive state closely enough.
-var Reflexes = (function(){
+/**
+	Reflexes are actions that can be 'hot-triggered' every cycle. They map certain input stimuli vectors to set output responses,
+	Ex: a knee nerve stimulation of sufficient level triggers a kick reflex.
+
+	Reflexes also serve the hidden purpose of seeding the Memorizer with behaviors that it can build on.
+*/
+var Reflexes = (function(_reflexes){
+	var reflexes = _reflexes;
+	function init(){
+
+	}	
+	init();
+
+	function cycle(){
+		for(var i=0; i<reflexes.length;i++){
+			reflexes[i]();
+		}
+	}
 
 	return{
-
+		cycle:cycle
 	};
 });
