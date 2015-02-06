f99f2488f000283780a7c3f8ecf2ded6354da211
cnatale <Chris.Natale@gridpoint.com>
2015-02-05 16:19:58 -0800

    more memorizer unit tests

--- tests/ihtaiSpec.js
+++ tests/ihtaiSpec.js
@@ -30,26 +30,17 @@ describe('ihtai', function(){
 	});
 
 	describe('Memorizer', function(){
-		var memorizer;
+		var memorizer, memory, cluster;
 		beforeEach(function(){
 			memorizer = new Memorizer(100);
-		});
-		afterEach(function(){
 
-		});
-
-		it('should initialize memorizer', function(){
-			expect(memorizer.getHeight()).toBe(100);
-		});
-
-		it('should memorize vectors', function(){
-			var memory=[10,20,30,40,50];
-			var cluster = {id:0, stimuli:memory};
+			memory=[10,20,30,40,50];
+			cluster = {id:0, stimuli:memory};
 			memorizer.memorize(cluster);
 			var memory2=[50,40,30,20,10];
 			var cluster2 = {id:1, stimuli:memory2};
 			memorizer.memorize(cluster2);
-			var memory3=[25,25,25,25,25];
+			var memory3=[0,0,0,0,0];
 			var cluster3 = {id:2, stimuli:memory3};
 			memorizer.memorize(cluster3);
 
@@ -61,13 +52,35 @@ describe('ihtai', function(){
 			memorizer.memorize(cluster5);
 			var memory6=[40,40,40,40,40];
 			var cluster6 = {id:5, stimuli:memory6};
-			memorizer.memorize(cluster6);
+			memorizer.memorize(cluster6);			
+		});
+		afterEach(function(){
+
+		});
+
+		it('should initialize memorizer', function(){
+			expect(memorizer.getHeight()).toBe(100);
+		});
+
+		it('should memorize vectors', function(){
+
 
 			var levels = memorizer.getLevels();
 			expect(levels[0].series["3"].startState).toEqual([30,30,30,30,30]);
 			expect(levels[0].series["3"].secondState).toEqual([35,35,35,35,35]);
 			expect(levels[0].series["3"].endState).toEqual([40,40,40,40,40]);
 		});		
+
+		it('given a cluster, should return vector representing next action agent should take to minimize homeostasis differential', function(){
+			var res=memorizer.query(cluster);
+			/*
+			Explanation: the final memory in the series starting with cluster reaches perfect homeostasis
+			([0,0,0,0,0]). We expect res to equal the second vector in the series, [50,40,30,20,10]. This 
+			is the 'next step' that IHTAI thinks agent should take to get as close as possible to homeostasis
+			goal from current input stimuli.
+			*/
+			expect(res).toEqual([50,40,30,20,10]);
+		})
 
 	});
 
