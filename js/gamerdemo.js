737fd6df240ffd2859a2d540a72309455045a290
cnatale <Chris.Natale@gridpoint.com>
2015-03-02 17:33:12 -0800

    work on gamer demo, add asteroids open-source game to project

--- js/gamerdemo.js
+++ js/gamerdemo.js
@@ -9,74 +9,63 @@ require.config({
 });
 
 require([], function(){
-	//application starting point
-	var ihtai;
-
-	//////////// Load File Functionality /////////////////
-	if (window.File && window.FileReader && window.FileList && window.Blob) {
-	  // Great success! All the File APIs are supported.
-	} else {
-	  alert('The File APIs are not fully supported in this browser.');
-	}
-
-
-    ///////// init Ihtai /////////////
-    //add tiredness drive, add behavior that when tiredness=100, stop moving (to 'seed'
-   	// conservation of motion).
-	var hungerDrive={
-		hunger:100,
+	var pleasureDrive={
+		pleasure:100,
 		init:function(){
-			return this.hunger;
+			return this.pleasure;
 		},
-		cycle:function(stimuli){
-			if(stimuli[3] < 10){
-				if(this.hunger>0){
-					this.hunger--;
-				}
-				else
-					this.hunger=0;
-			}
-			else
-				if(this.hunger<100){
-					this.hunger+=.1;
+		cycle:function(stimuli, dt){
+			//TODO: update pleasure on score increase, slowly decrement over time
+			if(stimuli[0] > 50){
+				if(this.pleasure<100){
+					this.pleasure+= .01 * dt;
 				}
 				else{
-					this.hunger=100;
+					this.pleasure=100;
 				}
-
-			
-			$("#hunger").html("hunger: "+Math.floor(this.hunger));	
-			return this.hunger;
-		},
-		targetValue:0 //the goal value for hunger
-	};
-	var tiredDrive={
-		tiredness:0,
-		init:function(){
-			return this.tiredness;
-		},
-		cycle:function(stimuli){
-			if(stimuli[2] <= 50){
-				if(this.tiredness>0){
-					this.tiredness-=.1;
+			}
+			else{
+				if(this.pleasure>0){
+					this.pleasure-= .01 * dt;
 				}
 				else
-					this.tiredness=0;
+					this.pleasure=0;				
 			}
-			else
-				if(this.tiredness<100){
-					this.tiredness+=.1;
-				}
-				else{
-					this.tiredness=100;
-				}
 
-			$("#tiredness").html("tiredness: "+Math.floor(this.tiredness));	
-			return this.tiredness;
+			//clamp vals
+			this.pleasure=Math.min(this.pleasure, 100);
+			this.pleasure=Math.max(this.pleasure, 0);
+			$("#pleasure").html("hunger: "+Math.floor(this.pleasure));	
+			$("#avgPleasure").html("avg pleasure: "+Math.floor(ihtai.getProperties().drives.getAvgDriveValue()[0]));					
+			return this.pleasure;
 		},
-		targetValue:0 //the goal value for hunger
+		targetValue:100 //the goal value for pleasure
 	};
-	drives=[hungerDrive, tiredDrive];
+	var painDrive={
+		pain:0,
+		init:function(){
+			return this.pain;
+		},
+		cycle:function(stimuli,dt){
+			//increment pain on death, slowly decrement over time
+			if(stimuli[1] > 50){
+				this.pain+= 100 * dt;
+			}
+			else{
+				this.pain-= .01 * dt;
+			}
+
+			//clamp vals
+			this.pain=Math.min(this.pain, 100);
+			this.pain=Math.max(this.pain, 0);
+			$("#pain").html("pain: "+Math.floor(this.pain));	
+			$("#avgpain").html("avg pain: "+Math.floor(ihtai.getProperties().drives.getAvgDriveValue()[1]));				
+			return this.pain;
+		},
+		targetValue:0 //the goal value for pain
+	};
+	drives=[pleasureDrive, painDrive];
+
 
 	var reflexes = [{
 		matcher: function(stimuli){ /*move if pellet it detected*/
@@ -108,107 +97,30 @@ require([], function(){
 	}];
 
     ihtai = new Ihtai({
-		clusterCount:100000,/*value of 100,000 seems to allow for memorizer to take over quickly*/
-		vectorDim:6,/*number of iostimuli values + drives*/
-		memoryHeight:1000,/*how many steps ahead can ihtai look for an optimal stimuli trail?*/
+		clusterCount:80000,/*value of 100,000 seems to allow for memorizer to take over quickly*/
+		vectorDim:15/*108*/,/*number of iostimuli values + drives*/
+		memoryHeight:800,/*how many steps ahead can ihtai look for an optimal stimuli trail?*/
 		drivesList:drives,
 		reflexList:reflexes,
-		acceptableRange:600/*acceptable range for optimal stimuli is in square dist*/
-	});
-    /////////////////////////////////
-
-    //var res=ihtai.cycle([square?100:0,normalizedAngle?normalizedAngle:0,moveVel,normalizedDist]);
-    	//returns {reflexOutput:~, memorizerOutput:~}
-
-    //////////// image transformations /////////////////////
-    // from http://www.html5rocks.com/en/tutorials/canvas/imagefilters/
-	Filters = {};
-	Filters.getPixels = function(img) {
-		var c = this.getCanvas(img.width, img.height);
-		var ctx = c.getContext('2d');
-		ctx.drawImage(img);
-		return ctx.getImageData(0,0,c.width,c.height);
-	};
-
-	Filters.getCanvas = function(w,h) {
-		var c = document.createElement('canvas');
-		c.width = w;
-		c.height = h;
-		return c;
-	};
-
-	Filters.filterImage = function(filter, image, var_args) {
-		var args = [this.getPixels(image)];
-		for (var i=2; i<arguments.length; i++) {
-	    	args.push(arguments[i]);
-		}
-		return filter.apply(null, args);
-	};
-
-    Filters.grayscale = function(pixels, args) {
-		var d = pixels.data, output=[];
-		for (var i=0; i<d.length; i+=4) {
-	    	var r = d[i];
-	    	var g = d[i+1];
-	    	var b = d[i+2];
-	   		// CIE luminance for the RGB
-	    	// The human eye is bad at seeing red and blue, so we de-emphasize them.
-	    	//var v = 0.2126*r + 0.7152*g + 0.0722*b;
-	    	var v = (r + g + b)/3; //trying a simpler approach first
-	    	output.push(v);
-	  	}
-		return output;
-	};
+		acceptableRange:600,/*600*//*acceptable range for optimal stimuli is in square dist*/
+		backStimCt:1
+	});		
 
 
-    /////////////// ui /////////////////////////
-
-	$("#saveBtn").click(function(e){
-		var jsonString=ihtai.toJsonString('IhtaiDemo');
-		download(new Blob([jsonString]), "ihtaiSave.json", "text/plain");
-	});
-	$("#turnOffMemBtn").click(function(e){
-		var areTheyEnabled=ihtai.areMemoriesEnabled();
-		if(areTheyEnabled){
-			ihtai.enableMemories(false);
-			$('#turnOffMemBtn').html('Enable Memories');
-		}
-		else{
-			ihtai.enableMemories(true);
-			$('#turnOffMemBtn').html('Disable Memories');
-		}			
-	});
+	//TODO: add asteroids
+	var intervalID = window.setInterval(updateIhtai, 33);
 
 
-    function handleFileSelect(evt) {
-	    var files = evt.target.files; // FileList object
-	    var file= files[0];
-	    var reader = new FileReader();
-	    reader.onload= 
-	  	reader.onload=(function(theFile){
-	    	//call runapp with the content passed as param
-			return function(e) {
-				var ihtaiJsonString=e.target.result;
-				//runApp(res);
+	function updateIhtai(){
+		/*
+		signal 0: score increase
+		signal 1: death
+		signal 2: joystick (either left, up down by splitting 0-100 signal in thirds)
+		signal 3: fire button (space bar)
+		remaining signals: grayscale image data 
+		*/
 
-				/*TODO: 
-				-remove any circles currently in scene. (maybe not necessary since we're just
-				replacing its brain?)
-				-associate ihtai variable with new ihtai instance created through json string
-				*/
-				//instantiate ihtai with loaded file
-				ihtai= new Ihtai(ihtaiJsonString);
-        	};
-	  	})(file);
+		
 
-	  	reader.readAsText(file);
-    }
-
-    document.getElementById('files').addEventListener('change', handleFileSelect, false);
-    //////////////////////////////////////////////////////
-
-
-	});
-
-
+	}
 });
