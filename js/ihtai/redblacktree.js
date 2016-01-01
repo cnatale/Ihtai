93a92ad08a6e63ceb8b0be1efbf84eff52d1c1b8
Chris Natale <cnatale@gmail.com>
2015-12-31 23:45:57 -0800

    add insert functionality to red-black tree

---
+++ js/ihtai/redblacktree.js
@@ -0,0 +1,136 @@
+/*	Red-black self balancing binary search tree implementatin in JavaScript
+	2015 Chris Natale. Implementation based on CLRS */
+/*
+	Red-black properties:
+	1) every nde is either red or black
+	2) the root is black
+	3) every leaf (nilNode) is black
+	4) if a node is red, then both its children are black
+	5) for each node, all simple paths from the node to descendant leaves conain the same number of black nodes.
+*/
+
+function RedBlackTree(){
+	var BLACK=0, RED=1;
+	var nilNode={key:null, left:null, right:null, color:BLACK};
+	var root=nilNode;
+
+	function rotateLeft(x){
+		var y = x.right;
+		x.right = y.left;
+		if(y.left !== nilNode)
+			y.left.p = x;
+
+		y.p = x.p;
+		if(x.p === nilNode)
+			root = y;
+		else if(x===x.p.left) //if x is the parent's left node, make y the new left node of parent
+			x.p.left = y;
+		else x.p.right = y;	//if x is the parent's right node, make y the new right node of the parent
+		y.left = x;
+		x.p = y;
+	}
+
+	function rotateRight(y){
+		var x = y.left;
+		y.left=x.right;
+		if(x.right !== nilNode)
+			x.right.p = y;
+		
+		x.p=y.p;
+		if(y.p === nilNode)
+			root = x;
+		else if(y === y.p.left) //if y is the parent's left node, make x the new left node of parent
+			y.p.left = x;
+		else y.p.right = x; //if y is the parent's right node, make x the new right node of the parent
+		x.right = y;
+		y.p = x;
+	}
+
+	function max(){
+		
+
+		return node;
+	}
+	function min(){
+
+		return node;
+	}
+	function insert(z){
+		var y = nilNode;
+		var x = root;
+		while (x !== nilNode){
+			y=x;
+			if(z.key < x.key)
+				x = x.left;
+			else
+				x = x.right;
+		}
+
+		z.p = y;
+		if (y === nilNode)
+			root = z;
+		else if (z.key < y.key)
+			y.left = z;
+		else
+			y.right = z;
+
+		z.left = nilNode;
+		z.right = nilNode;
+		z.color = RED;
+		insertFixup(z);
+	}
+	function insertFixup(z){
+		while(z.p.color == RED){
+			if(z.p == z.p.p.left){ //z.p is grandparrent's left child
+				y = z.p.p.right;
+				if(y.color == RED){
+					z.p.color = BLACK;
+					y.color = BLACK;
+					z.p.p.color = RED;
+					z= z.p.p;
+				}
+				else{ 
+					if(z == z.p.right){
+						z = z.p;
+						rotateLeft(z);
+					}
+					z.p.color = BLACK;
+					z.p.p.color = RED;
+					rotateRight(z.p.p);
+				}
+			}
+			else{ //z.p is grandparent's right child 
+				//same as previous block but with 'right' and 'left' exchanged
+				y = z.p.p.left;
+				if(y.color == RED){
+					z.p.color = BLACK;
+					y.color = BLACK;
+					z.p.p.color = RED;
+					z= z.p.p;
+				}
+				else{
+					if(z == z.p.left){
+						z = z.p;
+						rotateRight(z);
+					}
+					z.p.color = BLACK;
+					z.p.p.color = RED;
+					rotateLeft(z.p.p);
+				}
+			}
+		}
+		root.color = BLACK;
+	}
+
+	function remove(id){
+		return node;
+	}
+
+	return{
+		max:max,
+		min:min,
+		insert:insert,
+		remove:remove,
+		root:root
+	}
+};
---
+++ tests/redBlackTreeSpec.js
@@ -0,0 +1,18 @@
+describe('ihtai utils', function(){
+	
+	describe('red-black self-balancing binary search tree', function(){
+		it('Should keep the path from the root to the farthest leaf no more than twice the length of the path from the root to the nearest leaf', function(){
+			expect(false).toBe(true);
+		});
+		it('Should return the smallest value stored in the tree', function(){
+			expect(false).toBe(true);
+		});
+		it('Should return the largest node value stored in the tree', function(){
+			expect(false).toBe(true);
+		});
+		it('Should return all values stored in the tree in order', function(){
+			
+		});
+
+	})
+})
