8c307bd000660278764f72e2fd79f2a59c1467c3
Chris Natale <cnatale@gmail.com>
2016-06-11 17:33:36 -0700

    begin work on redblacktreeadapter

--- js/ihtai/redblacktreeadapter.js
+++ js/ihtai/redblacktreeadapter.js
@@ -1,4 +1,21 @@
 var RedBlackTreeAdapter = (function() {
+	/*
+	Things we need to store:
+
+	fsUidTrees and ssIdTables allow for efficient storage and retrieval of memory sequence data
+
+	fsUidTrees: searcheable collections of memory chains, keyed off first state uid.
+		schema: each tree is keyed off dist from ideal drive state
+
+		
+	ssIdTables schema: each key is a memory uid that references:
+		-a hash table where each key is a second state output stimuli id for a memory chain, which references
+		 a uidTree node from the tree with the same memory uid as this table's key.
+
+	*/
+
+	FSUID_TABLES = 'fsUidTrees';
+	SSID_TABLES = 'ssIdTables';
 
 	function insert(storeName, tableId, nodeToAdd) {
 
@@ -17,6 +34,11 @@ var RedBlackTreeAdapter = (function() {
 	}
 
 	return {
-
+		FSUID_TABLES: FSUID_TABLES,
+		SSID_TABLES: SSID_TABLES,
+		insert: insert,
+		del: del,
+		createTable: createTable,
+		delTable: delTable
 	}
 });
